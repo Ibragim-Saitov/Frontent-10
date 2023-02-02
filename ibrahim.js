6e948a36fc2a58e76d26e845e8c84799cfb9dd42
Ibragim-Saitov <122826573+Ibragim-Saitov@users.noreply.github.com>
2023-02-02 17:04:00 +0300

    Create ibrahim.js

---
+++ ibrahim.js
@@ -0,0 +1,42 @@
+//1 Задание
+
+let a = ['lorem', 123, true, 'ipsum'];
+
+
+console.log(a.length);
+
+//2 Задание
+
+console.log(a [0]);
+console.log(a [a.length -1]);
+
+//3 Задание
+
+let em = [];
+em.push('lorem', 123, true, 'ipsum', 456,);
+
+//4 Задание
+
+em.unshift(true, 'ipsum', 456,);
+
+
+//5 Задание
+
+em.shift();
+em.splice(-2, 2);
+
+//6 Задание
+
+let number = [1, 3, 5, 7, 9,];
+number[0] *= 2;
+number[1] *= 2;
+number[2] *= 2;
+number[3] *= 2;
+number[4] *= 2;
+
+
+//7 Задание
+
+number.splice(2, 1);
+number.splice(0, 1, 'Ibragim');
+number.splice(1, 1, 'Saitov');
